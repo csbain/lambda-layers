eaa238d0e72009b4dc3e147998f3d93e94ca5bc4
Christopher Bain <christophersbain@gmail.com>
2019-04-21 23:35:05 +1000

    fixed bad gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd python-doc-inspection && docker build -t python-doc-inspection && cd ..
+cd python-doc-inspection && docker build -t python-doc-inspection . && cd ..
 
-./linux-amd64-img2lambda -n python-doc-inspection -i python-doc-inspection:latest -r us-west-2 --cr python3.6 --cr python3.7 --desc "Add on layer for python 3.6 and 3.7 to give extra binaries and pip libs relevant for document inspection/extraction"
+linux-amd64-img2lambda -n python-doc-inspection -i python-doc-inspection:latest -r us-west-2 --cr python3.6 --cr python3.7 --desc "Add on layer for python 3.6 and 3.7 to give extra binaries and pip libs relevant for document inspection/extraction"
