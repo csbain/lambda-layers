4e9751c510a946f12c574e0a61c9ee8cc0ce8dc9
Christopher Bain <christophersbain@gmail.com>
2019-04-22 15:57:20 +1000

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PWD
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PWD:PATH
 cd doc-utils-binaries && docker build -t doc-utils-binaries . && cd ..
 cd doc-utils-pythonlibs && docker build -t doc-utils-pythonlibs . && cd ..
 linux-amd64-img2lambda -n doc-utils-binaries -i doc-utils-binaries:latest -r us-west-2 --cr python3.6 --cr python3.7 --desc "Add on layer for python 3.6 and 3.7 to give extra binaries relevant for document inspection/extraction Part 1 of 2"
