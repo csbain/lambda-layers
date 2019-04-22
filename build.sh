bd871cafb63f6ba7cb254aff7367ca14b919631a
Christopher Bain <christophersbain@gmail.com>
2019-04-22 16:33:10 +1000

    circleci support

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PWD:PATH
+get https://github.com/awslabs/aws-lambda-container-image-converter/releases/download/0.1.0/linux-amd64-img2lambda
+chmod +x linux-amd64-img2lambda
 cd doc-utils-binaries && docker build -t doc-utils-binaries . && cd ..
 cd doc-utils-pythonlibs && docker build -t doc-utils-pythonlibs . && cd ..
 linux-amd64-img2lambda -n doc-utils-binaries -i doc-utils-binaries:latest -r us-west-2 --cr python3.6 --cr python3.7 --desc "Add on layer for python 3.6 and 3.7 to give extra binaries relevant for document inspection/extraction Part 1 of 2"
